da20d3171f3b58c0a0a4e6206b9694f50eaffb04
There Haru <there.haru@gmail.com>
2023-03-02 08:43:14 +0800

    Downgrade To PHP V7

---
+++ database/migrations/2023_03_02_001034_create_complaints_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComplaintsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('complaints', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('response_id');
+            $table->string('nama');
+            $table->string('nik');
+            $table->text('aduan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('complaints');
+    }
+}
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,37 @@
+<div class="wrapper">
+    <header>
+    <div class="container">
+    <div class="header-data">
+    <div class="logo">
+    <a href="index.php" title=""><img src="images/logo.png" alt=""></a>
+    </div><!--logo end-->
+    <div class="search-bar navbar">
+    <h1 class="text-center text-white h6">Aplikasi Pengaduan Masyarakat</h1>
+    </div><!--search-bar end-->
+    <nav>
+    <ul>
+    <li>
+        <a href="/" title="">
+            <span><img src="images/icon1.png" alt=""></span>
+            Home
+        </a>
+    </li>
+    <li>
+        <a href="/laporan" title="">
+            <span><img src="images/icon9.png" alt=""></span>
+            Laporan
+        </a>
+    </li>
+    <li>
+        <a href="/profil" title="">
+            <span><img src="images/icon4.png" alt=""></span>
+            Profil
+        </a>
+    </li>
+    </ul>
+    </nav><!--nav end-->
+
+    
+</div><!--header-data end-->
+</div>
+</header><!--header end-->
